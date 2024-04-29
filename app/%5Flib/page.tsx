888c2451209f185d4a05af47e4784beadc794ad6
Sabari-1234 <sabarinathan5802@gmail.com>
2024-04-29 15:30:54 +0530

    private folder with _ and _ in route with %5F

---
+++ app/%5Flib/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>page shoud be routed with _lib</h1>
+    </div>
+  );
+};
+
+export default page;
---
+++ app/_lib/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>i am private </h1>
+    </div>
+  );
+};
+
+export default page;
