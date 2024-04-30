98ba111fe9d8061869ac095b86d5867c26d13ceb
Sabari-1234 <sabarinathan5802@gmail.com>
2024-04-30 11:41:48 +0530

    deeper the error.tsx file more granular on which content to display and which would be replaced with error.tsx more accurately

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { throws } from "assert";
 import { Metadata } from "next";
 import Link from "next/link";
@@ -11,20 +10,13 @@ type props = {
   };
 };
 
-// export const generateMetadata = ({ params }: props): Metadata => {
-//   return {
-//     title: `product-${params.productId}`,
-//   };
-// };
-
-const randomaNumber = (count: number) => {
-  return Math.floor(Math.random() * count);
+export const generateMetadata = ({ params }: props): Metadata => {
+  return {
+    title: `product-${params.productId}`,
+  };
 };
+
 const page = ({ params }: props) => {
-  const num = randomaNumber(2);
-  if (num == 1) {
-    throw new Error("product id is not in use ");
-  }
   return (
     <div>
       <h1>product -{params.productId}</h1>
